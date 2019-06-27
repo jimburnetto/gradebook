554eb3ce91549bbecb2341695b89120491553d65
Jim Burnett <jburnett@tenaska.com>
2019-06-27 14:53:37 -0500

    add files

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book{
+        public Book(string name)
+        {
+            grades = new List<double>();   
+            Name = name;            
+            
+        }
+        public void AddGrade(double grade){
+          grades.Add(grade);
+        }
+
+        public Statistics getStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low );
+                result.Average += grade;
+            }
+            
+            result.Average /= grades.Count;
+            return result;
+
+        }
+
+        private List<double> grades = new List<double>();          
+        public string Name;     
+    }
+}
---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+
+
+namespace GradeBook.Tests
+{
+    public class TypeTests
+    {
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+            //act
+            
+
+            //assert
+            Assert.Equal("Book 1",book1.Name);
+            Assert.Equal("Book 2",book2.Name);
+            Assert.NotSame(book1,book2);
+               
+        }
+        [Fact]
+        public void TwoVariablesCanReferenceSameObject()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            //act
+            
+
+            //assert
+            Assert.Same(book1,book2);
+            Assert.True(Object.ReferenceEquals(book1,book2));  //values in these variables point to the same object
+               
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
+        }
+    }
+}
